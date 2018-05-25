043ece55811a76fcf6b2f4a32055668e6764fb06
Mads Maretty Sønderup <kontakt@madsmaretty.dk>
2018-05-25 18:37:23 +0200

    Starting on Station scaler

---
+++ HatMaker/Assets/Station_Scaler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Station_Scaler : MonoBehaviour {
+
+    public GameObject SnapPoint;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Hat")
+        {
+            Rigidbody ObjRB = col.gameObject.GetComponent<Rigidbody>();
+            ObjRB.velocity = Vector3.zero;
+            ObjRB.angularVelocity = Vector3.zero;
+
+            Transform ObjTra = col.gameObject.GetComponent<Transform>();
+            ObjTra.transform.position = SnapPoint.transform.position;
+        }
+    }
+}
