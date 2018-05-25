be95d750c24246e1036a285e06d76369c3d6479f
Bjeck <bjarke@larsen-dk.dk>
2018-05-25 16:20:53 +0200

    hats and movement!

---
+++ HatMaker/Assets/Hat.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hat : MonoBehaviour {
+
+    SpringJoint springjoint;
+    Player connectedPlayer;
+    public float maxDistance = 10f;
+
+    private void Start()
+    {
+        springjoint = GetComponent<SpringJoint>();
+    }
+
+    private void Update()
+    {
+       
+    }
+
+    public void OnInteract(object player)
+    {
+        print("i'm a hat and I want to interact with you!");
+
+        connectedPlayer = (Player)player;
+
+        if(connectedPlayer != null)
+        {
+            springjoint.connectedBody = connectedPlayer.GetComponent<Rigidbody>();
+            springjoint.connectedAnchor = connectedPlayer.transform.position + connectedPlayer.transform.forward;
+
+
+        }
+
+
+    }
+}
--- HatMaker/Assets/Player.cs
+++ HatMaker/Assets/Player.cs
@@ -12,7 +12,9 @@ public class Player : MonoBehaviour {
 
     public float speed = 5f;
 
+    public float maxVelocityMagnitude = 30f;
 
+    Quaternion rotation;
 	// Use this for initialization
 	void Start () {
         rigidbody = GetComponent<Rigidbody>();
@@ -21,9 +23,43 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        velocity = new Vector3(hInput.GetAxis(controller + "Horizontal"), hInput.GetAxis(controller + "Vertical"),0);
+        velocity = new Vector3(hInput.GetAxis(controller + "Horizontal"), 0 , -hInput.GetAxis(controller + "Vertical"));
         velocity *= speed;
-        rigidbody.velocity = velocity;
 
-	}
+        if(rigidbody.velocity.magnitude < maxVelocityMagnitude)
+        {
+            rigidbody.AddForce(velocity, ForceMode.VelocityChange);
+        }
+
+
+        if(rigidbody.velocity.magnitude > 0.5f)
+        {
+            transform.LookAt(transform.position + rigidbody.velocity);
+        //rigidbody.angularVelocity = Vector3.zero;
+            rotation.eulerAngles = new Vector3(0, transform.rotation.eulerAngles.y, 0);
+
+            transform.rotation = rotation;
+        }
+
+
+        if (hInput.GetButtonDown(controller + "Use"))
+        {
+            TryUse();
+        }
+
+    }
+
+
+    void TryUse()
+    {
+        RaycastHit[] hits;
+        hits = Physics.SphereCastAll(transform.position, 1, transform.forward,3f);
+
+        for (int i = 0; i < hits.Length; i++)
+        {
+            hits[i].transform.gameObject.SendMessage("OnInteract",this,SendMessageOptions.DontRequireReceiver);
+        }
+    }
+
+
 }
