67b94cdeaaebd7c5c3db26ea51ee11021a378954
Mads Maretty Sønderup <kontakt@madsmaretty.dk>
2018-05-25 16:40:44 +0200

    New machine. spawning hats.

---
+++ HatMaker/Assets/Interact.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interact : MonoBehaviour {
+
+    public UnityEvent TriggerObject;
+
+    public void OnInteract(GameObject G){
+        
+        TriggerObject.Invoke();
+    }
+
+}
