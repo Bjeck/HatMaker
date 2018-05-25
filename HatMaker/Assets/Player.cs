b063c6920fe0535d4b1465db3b45cfee597561b7
Bjeck <bjarke@larsen-dk.dk>
2018-05-25 13:39:42 +0200

    input beginnings

---
+++ HatMaker/Assets/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public string controller;
+
+    Rigidbody rigidbody;
+
+    Vector3 velocity;
+
+    public float speed = 5f;
+
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        velocity = new Vector3(hInput.GetAxis(controller + "Horizontal"), hInput.GetAxis(controller + "Vertical"),0);
+        velocity *= speed;
+        rigidbody.velocity = velocity;
+
+	}
+}
