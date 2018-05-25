abce7b82c73ac81424179aa4dd7674378106afd3
Bjeck <bjarke@larsen-dk.dk>
2018-05-25 21:51:29 +0200

    orders done, now doing ui

--- HatMaker/Assets/GameManager.cs
+++ HatMaker/Assets/GameManager.cs
@@ -4,5 +4,29 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    public int playerCount = 1;
+
+    public List<Player> playersAtStart = new List<Player>(); //this is 4 ppl long. because i'm lazy
+
+    public Orders orders;
+
+    private void Awake()
+    {
+
+        int playerCountReverse = 4 - playerCount;
+
+        for (int i = playerCountReverse; i >= playerCount; i--)
+        {
+            Player p = playersAtStart[i];
+            playersAtStart.Remove(p);
+            Destroy(p.gameObject);
+
+        }
+
+        orders.players = playersAtStart;
+        orders.StartGame();
+
+    }
+
 
 }
