043bef870a48158af34c8d34c34a50f2d0b4f10c
Mads Maretty Sønderup <kontakt@madsmaretty.dk>
2018-05-26 11:11:50 +0200

    Starting on making a customer line.

--- HatMaker/Assets/Customer.cs
+++ HatMaker/Assets/Customer.cs
@@ -5,9 +5,24 @@ using UnityEngine.AI;
 
 public class Customer : MonoBehaviour {
 
-    private NavMeshAgent NMA; 
+    private NavMeshAgent NMA;
+    private CustomerLine CL;
+    public Transform TargetTransform;
 
-    public void Start(){
+    public void Start()
+    {
         NMA = GetComponent<NavMeshAgent>();
+        CL = GameObject.Find("CustomerLine").GetComponent<CustomerLine>();
+        CustomerLine.CustomerPositionClass CPC = CL.AskForPosition(gameObject);
+        if(CPC != null){
+            TargetTransform = CPC.Position.transform;
+            NMA.SetDestination(TargetTransform.position);
+        }
+
     }
+
+    //public void Update(){
+
+       
+    //}
 }
--- HatMaker/Assets/CustomerLine.cs
+++ HatMaker/Assets/CustomerLine.cs
@@ -1,13 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class CustomerLine : MonoBehaviour {
 
-    public List<GameObject> CustomerPositions;
+    [System.Serializable]
+    public class CustomerPositionClass
+    {
+        public GameObject Position;
+        public GameObject OccupiedBy;
 
-    void Start(){
-
-        //CustomerPositions = GameObject.find
+        public CustomerPositionClass(GameObject Pos, GameObject Who)
+        {
+            Position = Pos;
+            OccupiedBy = Who;
+        }
     }
+
+    [Header("Customer Positions")]
+    public List<CustomerPositionClass> HandlePosition;
+
+    [Header("Customer Positions")]
+    public List<CustomerPositionClass> WaitingPosition;
+
+    public CustomerPositionClass AskForPosition(GameObject customer){
+
+        int GetPosition = 99;
+        for (int i = 0; i < HandlePosition.Count; i++)
+        {
+            if (HandlePosition[i].OccupiedBy == null)
+            {
+                GetPosition = i;
+                break;
+            }
+        }
+
+        if(GetPosition != 99){
+            return HandlePosition[GetPosition];
+        }else{
+            return null;
+        }
+    }
+
 }
