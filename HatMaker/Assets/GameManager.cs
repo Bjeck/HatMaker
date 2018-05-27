071f703d4855cd873bac8f6a6ae4afaa35b5c679
Mads Maretty Sønderup <kontakt@madsmaretty.dk>
2018-05-27 13:49:09 +0200

    Sounds

--- HatMaker/Assets/GameManager.cs
+++ HatMaker/Assets/GameManager.cs
@@ -46,6 +46,7 @@ public class GameManager : MonoBehaviour {
 
     public void EndGame()
     {
+        customerLine.AngryCrowd.Stop();
         Victory.Play();
         Camera.main.gameObject.GetComponent<CameraControl>().MoveToScoreBoard();
 
--- HatMaker/Assets/Materials/Scripts/UnityEventOnInteraction.cs
+++ HatMaker/Assets/Materials/Scripts/UnityEventOnInteraction.cs
@@ -6,9 +6,13 @@ using UnityEngine.Events;
 public class UnityEventOnInteraction : MonoBehaviour
 {
     public UnityEvent OnInteraction;
+    public AudioSource ClickSound;
 
     public void OnInteract(object payload)
     {
+        if(ClickSound != null){
+            ClickSound.Play();
+        }
         OnInteraction.Invoke();
     }
 }
